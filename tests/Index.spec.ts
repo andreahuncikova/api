6f39e0ca9665cd923ae809d204b8044401c29a15
andreahuncikova <andhun01@easv365.dk>
2026-05-19 11:35:53 +0200

    fix remove DB connection from test setup

--- tests/Index.spec.ts
+++ tests/Index.spec.ts
@@ -1,5 +1,4 @@
 /// <reference types="node" />
-// process.env.NODE_ENV = 'test';
 import { test } from '@playwright/test';
 
 // Test Collections
@@ -10,33 +9,6 @@ import authLoginTests from './Auth.login.spec';
 import booksPublicTests from './Books.public.spec';
 import booksProtectedTests from './Books.protected.spec';
 import booksE2eTests from './Books.e2e.spec';
-
-// Project imports
-import { userModel } from "../src/models/userModel";
-import { BookModel } from "../src/models/bookModel";
-import { connect, disconnect } from '../src/repository/database';
-import dotenvFlow from "dotenv-flow";
-dotenvFlow.config();
-
-function setup() {
-    test.beforeAll(async () => {
-            await connect();
-            await userModel.deleteMany({});
-            await BookModel.deleteMany({});
-    });
-
-    test.afterAll(async () => {
-        try {
-            await connect();
-            await userModel.deleteMany({});
-            await BookModel.deleteMany({});
-        } finally {
-            await disconnect();
-        }
-    });
-}
-
-setup();
 
 // Run tests sequentially
 test.describe(healthTests);
