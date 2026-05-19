dadc71e0bc51218a45a416a2892c7af5233e718b
andreahuncikova <andhun01@easv365.dk>
2026-05-19 11:32:37 +0200

    deploy website only if tests have passed and lint fix

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import path from "path";
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import dotenvFlow from "dotenv-flow";
 import { connect } from './repository/database';
 import routes from './routes';
--- tests/Auth.login.spec.ts
+++ tests/Auth.login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, request } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 const BASE_URL = "/api"; // Použitie relatívnej cesty
 
